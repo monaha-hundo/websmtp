c913f51c1aa4e93d993c09636cb2c45756c88482
Yvan Dumont <microsoft@yvan.email>
2024-03-04 04:29:45 -0500

    forgot to include changes between the last two commits

--- websmtp/IReadableMessageStore.cs
+++ websmtp/IReadableMessageStore.cs
@@ -3,7 +3,7 @@ using websmtp.Database.Models;
 
 public interface IReadableMessageStore
 {
-    public ListResult Latest(int page , int perPage, bool onlyNew, bool showTrash, string filter);
+    public ListResult Latest(int page , int perPage, bool onlyNew, bool showTrash, bool showSpam, string filter);
     public Message Single(Guid msgId, bool includeRaw = false);
     public void MarkAsRead(Guid msgId);
     public void Delete(Guid msgId);
