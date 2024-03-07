4773c28551f73471ce3aaa497d1f373dfcbf68e2
Yvan Dumont <github@yvan.email>
2024-03-07 12:33:50 -0500

    added basic "filter" test, added a way to store datetimeoffset in sqlite

--- tests/Automated.cs
+++ tests/Automated.cs
@@ -281,4 +281,143 @@ public class Basic
             tokenSource2.Cancel();
         }
     }
+
+
+    [TestMethod]
+    public void Filter()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+        using var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+        var sendMailService = scope.ServiceProvider.GetRequiredService<SendMailService>();
+        var msgStore = scope.ServiceProvider.GetRequiredService<IReadableMessageStore>();
+        var env = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+
+        Console.WriteLine("Generating test data...");
+        var testEmailCount = 20;
+
+        var fromEmailAddrs = new Faker<MailAddress>()
+            .CustomInstantiator(f =>
+            {
+                var fn = f.Name.FirstName();
+                var ln = f.Name.LastName();
+                var prov = "websmtp.local";
+                var email = f.Internet.Email(fn, ln, prov, "");
+                return new MailAddress(email, $"{fn} {ln}");
+            })
+            .Generate(15);
+
+        var toEmailAddrs = new Faker<MailAddress>()
+            .CustomInstantiator(f =>
+            {
+                var fn = f.Name.FirstName();
+                var ln = f.Name.LastName();
+                var prov = f.Internet.DomainName();
+                var email = f.Internet.Email(fn, ln, prov, "");
+                return new MailAddress(email, $"{fn} {ln}");
+            })
+            .Generate(15);
+
+        var files = new Faker<FakeFile>()
+            .CustomInstantiator((f) => new FakeFile(
+                f.Lorem.Slug() + "." + f.Lorem.Word(),
+                new MemoryStream(System.Text.Encoding.UTF8.GetBytes(f.Lorem.Paragraphs(10)))
+            ))
+            .Generate(1);
+
+        var testRunId = Guid.NewGuid().ToString("N");
+
+        var messages = new Faker<MailMessage>()
+            .CustomInstantiator(f =>
+            {
+                var message = new MailMessage(f.PickRandom(fromEmailAddrs), f.PickRandom(toEmailAddrs));
+
+                for (int i = 0; i < f.Random.Number(0, 10); i++)
+                {
+                    var file = f.PickRandom(files);
+                    message.Attachments.Add(new Attachment(file.Content, file.FileName));
+                }
+
+                for (int i = 0; i < f.Random.Number(0, 10); i++)
+                {
+                    message.CC.Add(f.PickRandom(toEmailAddrs));
+                }
+
+                for (int i = 0; i < f.Random.Number(0, 10); i++)
+                {
+                    message.Bcc.Add(f.PickRandom(toEmailAddrs));
+                }
+
+                return message;
+            })
+            .RuleFor(m => m.Body, (f) => f.Lorem.Paragraphs(2))
+            .RuleFor(m => m.Subject, (f) => $"Test run_id={testRunId} msg_id={Guid.NewGuid()}")
+            .RuleFor(m => m.Priority, (f) => f.Random.Enum<MailPriority>())
+            .Generate(testEmailCount);
+
+        var msgGuidToFind = Guid.NewGuid().ToString();
+        messages[0].Subject = $"Test run_id={testRunId} msg_id={msgGuidToFind}";
+
+        var domains = toEmailAddrs.Select(m => m.Host).ToList();
+
+        var mimeMessages = messages.Select(m => MimeMessage.CreateFromMailMessage(m)).ToList();
+
+        var domain = "websmtp.local";
+        var dkimDomainConfigSection = config.GetSection("DKIM:Domains");
+        var domainsConfigs = dkimDomainConfigSection.GetChildren();
+        var domainConfig = domainsConfigs.Where(s => s.GetValue<string>("Name") == domain).SingleOrDefault();
+        if (domainConfig == null) throw new Exception($"Trying to sign a message for an unconfigured email domain: '{domain}'.");
+
+        var selector = domainConfig.GetValue<string>("Selector") ?? throw new Exception("Missing DKIM:Domain:Selector config key.");
+        var privateKeyFilename = domainConfig.GetValue<string>("PrivateKey") ?? throw new Exception("Missing DKIM:Domain:PrivateKey config key.");
+        var publicKeyFilename = privateKeyFilename.Replace("private", "pub").Replace("pem", "der");
+        var publicKey = Convert.ToBase64String(File.ReadAllBytes(publicKeyFilename));
+
+        var dnsPort = config.GetValue<int>("DNS:Port");
+
+        var masterFile = new MasterFile();
+        masterFile.AddIPAddressResourceRecord("websmtp.local", "127.0.0.1");
+        masterFile.AddMailExchangeResourceRecord("websmtp.local", 10, "localhost");
+        masterFile.AddTextResourceRecord("dkim._domainkey.websmtp.local", "dkim", "v=DKIM1; p=" + publicKey);
+        masterFile.AddTextResourceRecord("websmtp.local", "v", "spf1 +all");
+
+        domains.GroupBy(d => d).Select(dG => dG.FirstOrDefault()).ToList().ForEach(dmn =>
+        {
+            masterFile.AddIPAddressResourceRecord(dmn, "127.0.0.1");
+            masterFile.AddMailExchangeResourceRecord(dmn, 10, "localhost");
+            masterFile.AddTextResourceRecord(dmn, "v", "spf1 +all");
+        });
+        using var server = new DnsServer(masterFile);
+        var tokenSource2 = new CancellationTokenSource();
+        CancellationToken ct = tokenSource2.Token;
+        Task.Run(async () =>
+        {
+            await server.Listen(dnsPort, IPAddress.Parse("127.0.0.1"));
+        }, ct);
+
+        try
+        {
+            Console.WriteLine($"Sending {testEmailCount} emails...");
+
+            mimeMessages.ForEach(sendMailService.SendMail);
+
+            var filterResult = msgStore.Latest(1, 1000, false, false, false, msgGuidToFind);
+
+            Assert.IsTrue(filterResult.Count == 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Test failed, exception while sending emails:");
+            Console.WriteLine(ex);
+            //throw;
+            Assert.Fail("Test failed, exception while sending emails.");
+        }
+        finally
+        {
+            files?.ForEach(f => f.Content?.Dispose());
+            mimeMessages?.ForEach(m => m?.Dispose());
+            messages?.ForEach(m => m?.Dispose());
+            tokenSource2.Cancel();
+        }
+    }
 }
