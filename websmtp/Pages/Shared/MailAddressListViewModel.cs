e607acde79399464642d8db86fcd22991d745c8d
Yvan <github@yvan.email>
2024-03-09 10:15:58 -0500

    w.i.p. added fancy destination editor

---
+++ websmtp/Pages/Shared/MailAddressListViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace websmtp;
+
+public class MailAddressListViewModel
+{
+    public string ContainerId { get; set; }
+    public string Id { get; set; }
+    public string ContainerClass { get; set; }
+    public string Label { get; set; }
+    public bool ShowCCandBCC { get; set; }
+}
