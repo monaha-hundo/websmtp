02507462f791507c1c2451dfe44ccd0b53b68a71
Yvan <github@yvan.email>
2024-03-10 12:15:53 -0400

    csp report ON, reviewed landing & login

--- websmtp/Program.cs
+++ websmtp/Program.cs
@@ -10,12 +10,31 @@ Startup.ConfigureServices(builder);
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    var cspHeaderName = "Content-Security-Policy-Report-Only";
+    var csp = new Dictionary<string, List<string>>()
+    {
+        {"default-src", new List<string>{"self"}},
+        {"connect-src", new List<string>{"self"}},
+        {"script-src", new List<string>{"self"}},
+        {"img-src", new List<string>{"self"}},
+        {"style-src", new List<string>{"self"}},
+        {"frame-src", new List<string>{"self"}}
+    };
+    var cspHeaderValue = string.Join("; ", csp.Keys.Select(c => $"{c} {string.Join(' ', csp[c].Select(s => "'" + s + "'"))}"));
+
+    context.Response.Headers.Append(cspHeaderName, cspHeaderValue);
+    await next.Invoke();
+    // Do logging or other work that doesn't write to the Response.
+});
+
 Startup.ConfigureAppPipeline(app);
 Startup.MapEndpoints(app);
 
 CommandLine.ParseModifiersArgs(args, app);
 
-if(app.Environment.IsEnvironment("Test"))
+if (app.Environment.IsEnvironment("Test"))
 {
     CommandLine.MigrateDatabase(app);
 }
