24c8ba8b59021cf30f88ca7b6ccdf4bfc89cf11d
Yvan <github@yvan.email>
2024-03-15 08:35:17 -0400

    fixed double save bug on incoming mail also removed bogus from the test projects references

---
+++ tests/Faker.cs
@@ -0,0 +1,11 @@
+﻿namespace Tests;
+
+static public class Faker
+{
+    public static T PickRandom<T>(this IList<T> list)
+    {
+        var rI = Random.Shared.Next(0, list.Count - 1);
+        return list[rI];
+    }
+
+}
