124ee6d8815f7c2de7e583c1c4e39586080ab237
Yvan <github@yvan.email>
2024-04-18 07:37:49 -0400

    small improvements: - sidebar stats reloaded after operations - view message HTML no more quirks mode - added #index hash to sidebar links (fix iphone history) - fix new message in Account & Users page - refactored and remove interval from view message iframe resize logic - login redirect to /inbox#index - docker file now uses non root user

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 # syntax=docker/dockerfile:1
 FROM yvansolutions/spamassassin:latest
 
+RUN useradd -m websmtp 
+RUN apt install -y libcap2-bin
+
 # filesystem
 ADD ./build/ /
+RUN setcap 'cap_net_bind_service=+ep' ./websmtp
+
 RUN mkdir -p /certificates/
+RUN chown websmtp:websmtp /certificates/
 VOLUME /certificates/
 
 ENV ASPNETCORE_ENVIRONMENT=Production
@@ -14,4 +20,5 @@ EXPOSE 25
 EXPOSE 5000
 
 # launch
+USER websmtp
 CMD ["./websmtp", "--migrate-database"]
--- websmtp/wwwroot/js/view.js
+++ websmtp/wwwroot/js/view.js
@@ -124,22 +124,29 @@ function showRawMsg() {
     window.parent.openRawMsg(msgId);
 }
 
-var viewResizeInterval = setInterval(() => {
-    let height = document.documentElement.scrollHeight;
-    if (height == 0) return;
-    window.parent.document.querySelector('#msg-view').style.height = height + 'px';
-}, 100);
+// var innerViewResizeInterval = setInterval(() => {
+//     let iframeEl = document.getElementById('html-content');
+//     if (iframeEl == null) return;
+//     let height = iframeEl.contentDocument.documentElement.scrollHeight;
+//     if (height == 0) return;
+//     iframeEl.height = height;
+// }, 100);
 
-var innerViewResizeInterval = setInterval(() => {
+// document.addEventListener('unload', () => {
+//     console.log('clearing intervals');
+//     clearInterval(innerViewResizeInterval);
+// });
+
+document.getElementById('html-content').contentWindow.addEventListener('DOMContentLoaded', () => {
+    resizeIframe();
+ });
+
+function resizeIframe() {
     let iframeEl = document.getElementById('html-content');
-    if (iframeEl == null) return;
     let height = iframeEl.contentDocument.documentElement.scrollHeight;
-    if (height == 0) return;
     iframeEl.height = height;
-}, 100);
-
-document.addEventListener('unload', () => {
-    console.log('clearing intervals');
-    clearInterval(viewResizeInterval);
-    clearInterval(innerViewResizeInterval);
-});
+}
+// new ResizeObserver((entries) => {
+//     resizeIframe();
+// }).observe(document.body);
+// resizeIframe();
